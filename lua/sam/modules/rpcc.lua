7626ea8846bc3be6194a76346c0e832b6520d965
BuildTools <64500127+WindingDuke77@users.noreply.github.com>
2023-02-01 17:00:22 +0000

    fix issues with ulx and sam

--- lua/sam/modules/rpcc.lua
+++ lua/sam/modules/rpcc.lua
@@ -12,13 +12,18 @@ command.new("ForceStop")
     :AddArg("text", {hint = "reason", optional = true})
 
 	:OnExecute(function(ply, reason)
+        if rpcc.FullStop then 
+            return rpcc.SendToClient(ply, Color(255,0,0), "RPCC is already stopped.")
+        end
         rpcc.FullStop = true
         if reason then
             for k, v in pairs(player.GetAll()) do
                 rpcc.SendToClient(v, Color(255,0,0), "A staff member has stopped the Command with the following reason: " .. reason)
             end
         end
-        rpcc.MODULE:Log("{1} ran FullStop.", GAS.Logging:FormatPlayer(ply))
+        if rpcc.MODULE then
+            rpcc.MODULE:Log("{1} ran ForceStop.", GAS.Logging:FormatPlayer(ply))
+        end
 	end)
 :End()
 
--- lua/ulx/modules/sh/rpcc.lua
+++ lua/ulx/modules/sh/rpcc.lua
@@ -1,23 +1,20 @@
-CATEGORY_NAME = "RPCC"
+local catergory_name = "RPCC"
 
-local waittime = 60
-local lasttimeusage = -waittime
-function ulx.forcestop( calling_ply, reason )
-	if lasttimeusage + waittime > CurTime() then
-		ULib.tsayError( calling_ply, "Please wait " .. waittime .. " seconds before using this command again", true )
-		return
-	end
-
-	lasttimeusage = CurTime()
+local function forcestopFunc(calling, target, weapon, amount)
+    if rpcc.FullStop then
+        rpcc.SendToClient(calling, Color(255,0,0), "The command is already stopped.")
+        return
+    end
     rpcc.FullStop = true
     if reason then
         for k, v in pairs(player.GetAll()) do
             rpcc.SendToClient(v, Color(255,0,0), "A staff member has stopped the Command with the following reason: " .. reason)
         end
     end
-    rpcc.MODULE:Log("{1} ran FullStop.", GAS.Logging:FormatPlayer(ply))
+    if rpcc.MODULE then
+        rpcc.MODULE:Log("{1} ran FullStop.", GAS.Logging:FormatPlayer(ply))
+    end
 end
-local forcestop = ulx.command( CATEGORY_NAME, "ulx fullstop", ulx.forcestop, "!fullstop" )
-forcestop:defaultAccess( ULib.ACCESS_ADMIN )
-forcestop:addParam{ type=ULib.cmds.StringArg, hint="reason", ULib.cmds.optional }
+local forcestop = ulx.command( catergory_name, "ulx forcestop", forcestopFunc, "!forcestop" )
 forcestop:help( "Stop the current command." )
+forcestop:defaultAccess( ULib.ACCESS_SUPERADMIN )
